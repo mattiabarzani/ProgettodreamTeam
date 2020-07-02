889d24baf98d76130311cbc3d0bf00dad0459822
utente <utente@LAPTOP-FJ0POECK.homenet.telecomitalia.it>
2020-07-02 11:30:18 +0200

    aggiornato

--- ProgettodreamTeam/src/it/begear/corso/progettoDT/Main.java
+++ ProgettodreamTeam/src/it/begear/corso/progettoDT/Main.java
@@ -10,6 +10,8 @@ public class Main {
         System.out.println("scusate");
         
         System.out.println("Altra modifica!");
+        
+        System.out.println("Questa è la modifica di Lorenzo");
 	}
 
 }
